a39f36dff1588fd69beb05ccf7613d2f25b40151
basem osman <basem_osman@outlook.com>
2021-08-19 17:45:45 +0100

    updated footer and head

---
+++ footer.php
@@ -0,0 +1,53 @@
+<footer>
+    <div class="f-container">
+        <h1>Shopping</h1>
+        <div class="f-contents">
+            <div class="categories-list">
+                <h1>Product List</h1>
+                <ul>
+                    <li><a href="">Bakery</a></li>
+                    <li><a href="">Fruits</a></li>
+                    <li><a href="">Dairy</a></li>
+                    <li><a href="">Meat</a></li>
+                    <li><a href="">Fish</a></li>
+                </ul>
+            </div>
+            <div class="contactform">
+                <div class="support">
+                    <i class="fas fa-headset"></i>
+                    <div class="s-text">
+                        <h2>Support</h2>
+                        <p>012354699885</p>
+                    </div>
+                </div>
+                <div class="email">
+                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                    <e-text>
+                        <h2>Email</h2>
+                        <p>customer@example.com</p>
+                    </e-text>
+                </div>
+                <div class="social">
+                    <h1>Follows us</h1>
+                    <div class="s-icons">
+                        <a href=""><i class="fab fa-twitter"></i></a>
+                        <a href=""><i class="fab fa-facebook-f"></i></a>
+                        <a href=""><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright-content">
+        <div class="c-text">
+            <h1>
+                shopping
+                &copy All Rights Reserved. developed by <a href="">Basem osman</a>
+            </h1>
+        </div>
+        <div class="card-icon">
+            <i class="fab fa-cc-visa"></i>
+            <i class="fab fa-cc-mastercard"></i>
+        </div>
+    </div>
+</footer>
